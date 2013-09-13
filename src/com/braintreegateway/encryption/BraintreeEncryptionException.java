2f8f660b89d1103aa7a8b7087b48b5f63dc4097a
Aaron Bedra <aaron@aaronbedra.com>
2013-09-13 11:16:11 -0500

    Adds a throwable version of BraintreeEncryptionException

--- src/com/braintreegateway/encryption/BraintreeEncryptionException.java
+++ src/com/braintreegateway/encryption/BraintreeEncryptionException.java
@@ -4,4 +4,8 @@ public class BraintreeEncryptionException extends Exception {
     public BraintreeEncryptionException(String message) {
         super(message);
     }
+
+    public BraintreeEncryptionException(String message, Throwable cause) {
+	super(message, cause);
+    }
 }
